f5989fbca4b204c674c78549dd3e359cabf48f04
Igor <ingvardm@gmail.com>
2018-03-11 01:34:38 +0200

    added multiple callbacks for gyro sensor

--- src/sensors/gyro.js
+++ src/sensors/gyro.js
@@ -2,6 +2,12 @@ import { Accelerometer } from 'react-native-sensors'
 
 const G = null
 
+var callbacks = []
+
+function runCallbacks(data){
+    callbacks.forEach(callback => callback(data))
+}
+
 export async function init(){
     if(!G) G = await new Accelerometer({ updateInterval: 100 })
         .catch(e => console.log('Gyroscope not evailable', e))
@@ -9,9 +15,13 @@ export async function init(){
 }
 
 export function subscribe(callback){
-    G && G.subscribe(callback)
+    if(!G) return
+    callbacks.push(callback)
+    G.subscribe(runCallbacks)
 }
 
-export function stop(){
-    G && G.stop()
+export function unsubscribe(callback){
+    if(!G) return
+    callbacks.splice(Array.indexOf(callback),1)
+    if(!callbacks.length) G.stop()
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -15,7 +15,7 @@ export default class Main extends Component {
     }
 
     componentWillUnmount(){
-        gyro.stop()
+        gyro.unsubscribe(this.handleGyroUpdate)
     }
 
     async initializeSensors(){
